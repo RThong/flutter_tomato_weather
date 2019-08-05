26cf023784c03c56cc184bf575a41f4b22e60bd9
RThong <403216075@qq.com>
2019-08-05 17:41:50 +0800

    底部一周天气图标对接真实数据

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,48 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:bezier_chart/bezier_chart.dart';
+import 'models/weather.dart';
 import 'utils/helper.dart';
 
 class WeatherChart extends StatelessWidget {
+  List<Weather7DaysInfo> weather;
+
+  WeatherChart({this.weather}) {
+    // print(this.weather);
+    if (this.weather != null) {
+      // print(this
+      //     .weather
+      //     .map((_) => DataPoint<double>(value: double.parse(_.temp_day_c)))
+      //     .toList());
+      // print(this.weather.length);
+    }
+  }
+
   Widget build(BuildContext context) {
+    // List<double> a = [0, 5, 10, 15, 20, 25];
+    if (this.weather == null) {
+      return Container(width: 0, height: 0);
+    }
     return BezierChart(
       bezierChartScale: BezierChartScale.CUSTOM,
-      xAxisCustomValues: const [0, 5, 10, 15, 20, 25, 30, 35],
+      xAxisCustomValues:
+          new List<double>.generate(this.weather.length, (i) => 1.0 * i),
       footerValueBuilder: (double value) {
-        return "${value}\ndays";
+        if (value == 0.0) {
+          return '今';
+        } else if (value == 1.0) {
+          return '明';
+        } else {
+          final aaaa = this.weather[value.toInt()].week;
+          return aaaa.substring(aaaa.length - 1);
+        }
       },
-      series: const [
+      series: [
+        // 最高温度
         BezierLine(
           lineStrokeWidth: 2.0,
           lineColor: Color(0xfff4a875),
-          label: "m",
-          data: const [
-            DataPoint<double>(value: 10, xAxis: 0),
-            DataPoint<double>(value: 130, xAxis: 5),
-            DataPoint<double>(value: 50, xAxis: 10),
-            DataPoint<double>(value: 150, xAxis: 15),
-            DataPoint<double>(value: 75, xAxis: 20),
-            DataPoint<double>(value: 0, xAxis: 25),
-            DataPoint<double>(value: 5, xAxis: 30),
-            DataPoint<double>(value: 45, xAxis: 35),
-          ],
+          label: "℃",
+          data: this
+              .weather
+              .map((_) => DataPoint<double>(value: double.parse(_.temp_day_c)))
+              .toList(),
         ),
+        // 最低温度
         BezierLine(
           lineStrokeWidth: 2.0,
           lineColor: Color(0xff80bfe1),
-          label: "m",
-          data: const [
-            DataPoint<double>(value: 1, xAxis: 0),
-            DataPoint<double>(value: 80, xAxis: 5),
-            DataPoint<double>(value: 40, xAxis: 10),
-            DataPoint<double>(value: 110, xAxis: 15),
-            DataPoint<double>(value: 20, xAxis: 20),
-            DataPoint<double>(value: 0, xAxis: 25),
-            DataPoint<double>(value: 2, xAxis: 30),
-            DataPoint<double>(value: 20, xAxis: 35),
-          ],
+          label: "℃",
+          data: this
+              .weather
+              .map(
+                  (_) => DataPoint<double>(value: double.parse(_.temp_night_c)))
+              .toList(),
         ),
       ],
       config: BezierChartConfig(
+        contentWidth: MediaQuery.of(context).size.width * 0.7,
         // footerHeight: 40,
         verticalIndicatorStrokeWidth: 1.0,
         verticalIndicatorColor: textColor,
@@ -52,5 +70,62 @@ class WeatherChart extends StatelessWidget {
         // snap: false,
       ),
     );
+    // var i = 0;
+    // final a = new List<double>.generate(7, (i) => 5.0 * i);
+    // return this.weather == null
+    //     ? Container(width: 0, height: 0)
+    //     : BezierChart(
+    //         bezierChartScale: BezierChartScale.CUSTOM,
+    //         xAxisCustomValues: a,
+    //         // xAxisCustomValues: const [0, 5, 10, 15, 20, 25],
+    //         footerValueBuilder: (double value) {
+    //           return "今";
+    //         },
+    //         series: [
+    //           // 最高温度
+    //           BezierLine(
+    //             lineStrokeWidth: 2.0,
+    //             lineColor: Color(0xfff4a875),
+    //             label: "m",
+    //             // data: const [
+    //             //   DataPoint<double>(value: 10),
+    //             //   DataPoint<double>(value: 130),
+    //             //   DataPoint<double>(value: 50),
+    //             //   DataPoint<double>(value: 150),
+    //             //   DataPoint<double>(value: 75),
+    //             //   DataPoint<double>(value: 0),
+    //             // ],
+    //             data: this
+    //                 .weather
+    //                 .map((_) =>
+    //                     DataPoint<double>(value: double.parse(_.temp_day_c)))
+    //                 .toList(),
+    //           ),
+    //           // 最低温度
+    //           BezierLine(
+    //             lineStrokeWidth: 2.0,
+    //             lineColor: Color(0xff80bfe1),
+    //             label: "m",
+    //             data: const [
+    //               DataPoint<double>(value: 1),
+    //               DataPoint<double>(value: 80),
+    //               DataPoint<double>(value: 40),
+    //               DataPoint<double>(value: 110),
+    //               DataPoint<double>(value: 20),
+    //               DataPoint<double>(value: 0),
+    //             ],
+    //           ),
+    //         ],
+    //         config: BezierChartConfig(
+    //           contentWidth: MediaQuery.of(context).size.width * 0.7,
+    //           // footerHeight: 40,
+    //           verticalIndicatorStrokeWidth: 1.0,
+    //           verticalIndicatorColor: textColor,
+    //           showVerticalIndicator: true,
+    //           verticalIndicatorFixedPosition: false,
+    //           showDataPoints: false,
+    //           // snap: false,
+    //         ),
+    //       );
   }
 }
