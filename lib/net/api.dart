fc4b5d34d9a4860d444455905ef9a4d672e3dca4
RThong <403216075@qq.com>
2019-08-06 16:50:22 +0800

    通过传入不同的cityid来模拟渲染不同数据的效果

--- lib/net/api.dart
+++ lib/net/api.dart
@@ -1,12 +1,16 @@
 import 'package:dio/dio.dart';
 
 import 'base.dart';
+import 'dart:math';
 
 const baseURL = 'https://aider.meizu.com/app/weather/listWeather';
 
 getWeather() async {
+  // 对重新获取数据并渲染进行测试
+  var random = new Random().nextBool() ? '101210101' : '101240101';
+  print(random);
   Response response =
-      await dio.get(baseURL, queryParameters: {'cityIds': '101210101'});
+      await dio.get(baseURL, queryParameters: {'cityIds': random});
   return response.data;
   ;
 }
