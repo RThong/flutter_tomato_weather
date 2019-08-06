21edc7c951a320b6bb93697710ef7b995a7716e1
RThong <403216075@qq.com>
2019-08-06 16:55:35 +0800

    添加下拉重新获取数据的效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'dart:convert' show json;
-
 import 'package:flutter/material.dart';
-// import 'package:flutter_study/types/type.dart';
 import 'chart.dart';
 import 'net/api.dart';
-import 'package:dio/dio.dart';
 
 import 'info.dart';
 import 'package:flutter_study/models/weather.dart';
@@ -38,12 +34,15 @@ class _HomePageState extends State<HomePage> {
   // WeatherInfo realTimeInfo;
   WeatherNow info;
   List<Weather7DaysInfo> weather7daysInfo;
+  // 获取全局的RefreshIndicator的key
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
   bool isLoading = true;
 
-  _getWeather() async {
+  Future<Null> _getWeather() async {
     // WeatherRes res = await getWeather();
-    var b = await getWeather();
-    WeatherRes weather = new WeatherRes.fromJson(b);
+    var weatherJson = await getWeather();
+    WeatherRes weather = new WeatherRes.fromJson(weatherJson);
 
     setState(() {
       this.isLoading = false;
@@ -53,7 +52,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   WeatherNow _filterNowWeather(WeatherRes res) {
-    WeatherNow b = new WeatherNow(
+    WeatherNow result = new WeatherNow(
         img: res.value[0].realtime.img,
         temp: res.value[0].realtime.temp,
         wS: res.value[0].realtime.wS,
@@ -62,7 +61,7 @@ class _HomePageState extends State<HomePage> {
         quality: res.value[0].pm25.quality,
         temp_day_c: res.value[0].weathers[0].temp_day_c,
         temp_night_c: res.value[0].weathers[0].temp_night_c);
-    return b;
+    return result;
   }
 
   List<Weather7DaysInfo> _filterWeekWeather(WeatherRes res) {
@@ -71,101 +70,126 @@ class _HomePageState extends State<HomePage> {
     return list;
   }
 
+  Future<Null> _refresh() async {
+    // 延迟1秒获取获取数据
+    await Future.delayed(Duration(seconds: 1));
+    this._getWeather();
+  }
+
   @override
   void initState() {
     print('homepage');
     _getWeather();
-    // Timer.periodic(new Duration(seconds: 1), (timer) {
-    //   _getWeather();
-    // });
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     print(MediaQuery.of(context).size.width);
-    return ConstrainedBox(
-        constraints: BoxConstraints.expand(),
-        child: Stack(children: [
-          Container(
-              decoration: new BoxDecoration(
-                  gradient: const LinearGradient(
-                      begin: Alignment.topRight,
-                      stops: [
-                    .5,
-                    .8,
-                    1.0
-                  ],
-                      colors: [
-                    Color(0xFF36749d),
-                    Color(0xFF5aa9b8),
-                    Color(0xFF76b9b2)
-                  ])),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  // 顶部操作栏
+    return new RefreshIndicator(
+        key: _refreshIndicatorKey,
+        onRefresh: this._refresh,
+        displacement: 100,
+        child: ConstrainedBox(
+            constraints: BoxConstraints.expand(),
+            child: GestureDetector(
+                onVerticalDragStart: (DragStartDetails e) {
+                  //打印手指按下的位置(相对于屏幕)
+                  print("用户手指按下：${e.globalPosition}");
+                },
+                //手指滑动时会触发此回调
+                onVerticalDragUpdate: (DragUpdateDetails e) {
+                  print('正在滑动');
+                },
+                onVerticalDragEnd: (DragEndDetails e) {
+                  //打印滑动结束时在x、y轴上的速度
+                  //竖直速度大于0表示向下滑动就显示refreshIndicator，并获取数据
+                  if (e.velocity.pixelsPerSecond.dy > 0) {
+                    this._refreshIndicatorKey.currentState.show();
+                  }
+                },
+                child: Stack(children: [
                   Container(
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Color.fromRGBO(255, 255, 255, .5),
-                    child: Text('杭州市',
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.w100,
-                            decoration: TextDecoration.none)),
-                  ),
-                  // 实时天气信息
-                  Info(info: this.info),
-                  // 底部天气图标
-                  Container(
-                    height: 220,
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          height: 200,
-                          child: Container(
-                              // height: MediaQuery.of(context).size.height / 2,
-                              width: MediaQuery.of(context).size.width * 1,
-                              child: WeatherChart(
-                                weather: this.weather7daysInfo,
-                              )),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.75,
-                          child: Flex(
-                            direction: Axis.horizontal,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: this.weather7daysInfo == null
-                                ? []
-                                : this
-                                    .weather7daysInfo
-                                    .map((_) => Image.asset(
-                                          'images/${_.img}.png',
-                                          width: 20,
-                                          height: 20,
-                                        ))
-                                    .toList(),
+                      decoration: new BoxDecoration(
+                          gradient: const LinearGradient(
+                              begin: Alignment.topRight,
+                              stops: [
+                            .5,
+                            .8,
+                            1.0
+                          ],
+                              colors: [
+                            Color(0xFF36749d),
+                            Color(0xFF5aa9b8),
+                            Color(0xFF76b9b2)
+                          ])),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          // 顶部操作栏
+                          Container(
+                            height: 100,
+                            alignment: Alignment.center,
+                            // color: Color.fromRGBO(255, 255, 255, .5),
+                            child: Text('杭州市',
+                                textAlign: TextAlign.center,
+                                style: new TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w100,
+                                    decoration: TextDecoration.none)),
                           ),
+                          // 实时天气信息
+                          Info(info: this.info),
+                          // 底部天气图标
+                          Container(
+                            height: 220,
+                            margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  height: 200,
+                                  child: Container(
+                                      // height: MediaQuery.of(context).size.height / 2,
+                                      width:
+                                          MediaQuery.of(context).size.width * 1,
+                                      child: WeatherChart(
+                                        weather: this.weather7daysInfo,
+                                      )),
+                                ),
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.75,
+                                  child: Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: this.weather7daysInfo == null
+                                        ? []
+                                        : this
+                                            .weather7daysInfo
+                                            .map((_) => Image.asset(
+                                                  'images/${_.img}.png',
+                                                  width: 20,
+                                                  height: 20,
+                                                ))
+                                            .toList(),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                  // 通过isLoading状态来切换没有大小的container来显隐加载动画
+                  this.isLoading
+                      ? Container(color: Color.fromRGBO(255, 255, 255, .5))
+                      : Container(
+                          width: 0,
+                          height: 0,
                         )
-                      ],
-                    ),
-                  )
-                ],
-              )),
-          // 通过isLoading状态来切换没有大小的container来显隐加载动画
-          this.isLoading
-              ? Container(color: Color.fromRGBO(255, 255, 255, .5))
-              : Container(
-                  width: 0,
-                  height: 0,
-                )
-        ]));
+                ]))));
   }
 }
