d0039294d5ab17e129c0c5fb243f127c51987c6a
RThong <403216075@qq.com>
2019-08-13 18:26:34 +0800

    引入下雨下雪动画

--- lib/bg_ani.dart
+++ lib/bg_ani.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/weather_ani/rain_snow.dart';
 import 'weather_ani/cloud.dart';
 import 'weather_ani/sunny.dart';
 
@@ -14,6 +15,14 @@ class BgAni extends StatelessWidget {
                 margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
                 child: Cloud(),
               )
-            : Container(width: 0, height: 0);
+            : this.weatherNum >= 3 && this.weatherNum <= 12
+                ? Container(
+                    height: MediaQuery.of(context).size.height / 2,
+                    child: AnimationContainer(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                    ),
+                  )
+                : Container(width: 0, height: 0);
   }
 }
